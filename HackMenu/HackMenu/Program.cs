16bdb09f83f733f8b2ae8545b86e50ac6c20c04c
Kilovfhh <osamabinlacken000@gmail.com>
2025-08-31 19:54:05 -0400

    Add files via upload

---
+++ HackMenu/HackMenu/Program.cs
@@ -0,0 +1,5 @@
+﻿using HackMenu; 
+
+Renderer renderer = new Renderer(); // create instance of menu with overlay 
+renderer.Start().Wait(); // run it 
+
---
+++ HackMenu/HackMenu/Renderer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClickableTransparentOverlay;
+using ImGuiNET;
+
+namespace HackMenu
+{
+    public class Renderer : Overlay
+    {
+        // features 
+        public bool godMod = false;
+        public bool unlimitedAmmo = false;
+
+
+        protected override void Render() // here we render stuff 
+        {
+            ImGui.Begin("Menu");
+
+            ImGui.Checkbox("Godmode", ref godMod);
+            ImGui.Checkbox("UnlimitedAmmo", ref  unlimitedAmmo);
+
+            ImGui.End(); // This is required!
+
+
+        }
+    }
+}
